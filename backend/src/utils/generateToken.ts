1341f1a350e07426549ba16450eafae1deb668ab
suraj singh jadon <130445022+surajjadon@users.noreply.github.com>
2025-12-14 04:22:06 +0530

    Delete backend directory

--- backend/src/utils/generateToken.ts
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-const generateToken = (id: string): string => {
-  return jwt.sign({ id }, process.env.JWT_SECRET!, {
-    expiresIn: '30d', // The token will be valid for 30 days
-  });
-};
-
-export default generateToken;
